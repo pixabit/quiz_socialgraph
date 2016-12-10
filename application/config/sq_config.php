47e7eaa20123d57769c3c9737e0ba14c686dba1e
Robert Moran <robertmoran@MacBook-Pro-2.local>
2016-12-09 17:39:43 -0800

    initial commit

---
+++ application/config/sq_config.php
@@ -0,0 +1,7 @@
+<?php 
+		$sq_base_url='http://thaicultureclub.com/quiz//';
+		$sq_hostname='localhost';
+		$sq_dbname='learntha_quizlive';
+		$sq_dbusername='learntha_liveq';
+		$sq_dbpassword='3qa3a3w3weses2232aw##222';
+		?>
